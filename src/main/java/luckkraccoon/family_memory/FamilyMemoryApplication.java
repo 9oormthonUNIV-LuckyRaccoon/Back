a2c60ab63353eab9a6652f51033087f59e295c16
JuHyeonAh <jha08130930@gmail.com>
2025-09-06 07:38:27 +0900

    first commit

---
+++ src/main/java/luckkraccoon/family_memory/FamilyMemoryApplication.java
@@ -0,0 +1,13 @@
+package luckkraccoon.family_memory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyMemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyMemoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/luckkraccoon/family_memory/FamilyMemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package luckkraccoon.family_memory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyMemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
